e1fcf4b97bb73d7cc9441db92262488e1ae5b4a1
skadakar <dale.ole.morten@gmail.com>
2021-05-22 18:09:04 +0200

    Update start-taky.sh

--- start-taky.sh
+++ start-taky.sh
@@ -1,10 +1,15 @@
+#Folder creation
+mkdir -p /data/conf
+mkdir -p /data/logs
+mkdir -p /data/database
+
 #Env handling
 echo "Adding variables to relevant config files"
-source fts.sh
+source env.sh
 echo "Starting taky"
+
 #Print version because..
 taky --version
-
 
 #Starting taky
 taky -c /data/taky.conf -l debug
