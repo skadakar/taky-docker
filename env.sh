0847382b721134109fdae89218769ad8e6eb4afb
skadakar <dale.ole.morten@gmail.com>
2021-05-22 15:33:16 +0200

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,1 +1,4 @@
 # This file handles all env variables so it's easy to manipulate
+# /usr/local/lib/python3.8/dist-packages/taky-0.7.2.dev39+gf258121.d20210522-py3.8.egg/taky
+# /usr/local/lib/python3.8/dist-packages/taky-*.egg/taky
+
