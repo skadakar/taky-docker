e37f4a82013f18349a27a76a88458dc1934c3a5d
skadakar <dale.ole.morten@gmail.com>
2021-05-22 16:13:15 +0200

    Update start-taky.sh

--- start-taky.sh
+++ start-taky.sh
@@ -1,11 +1,14 @@
 #Setting folder stuff
 chmod -R 777 /data
+touch /data/taky.conf
 
 #Env handling
 echo "Adding variables to relevant config files"
 source fts.sh
 echo "Starting taky"
+#Print version because..
+taky --version
 
 
 #Starting taky
-taky
+taky -c /data/taky.conf -l debug
