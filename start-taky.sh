2c8a45ab121461142884e065bb1771e2082493e8
skadakar <dale.ole.morten@gmail.com>
2021-05-23 02:58:07 +0200

    Update start-taky.sh

--- start-taky.sh
+++ start-taky.sh
@@ -2,6 +2,7 @@
 echo "chownhack"
 chown -R taky:taky /data
 
+
 echo "Preparing folder structure"
 mkdir -p /data/conf
 mkdir -p /data/ssl
@@ -11,6 +12,15 @@ mkdir -p /data/database
 echo "Resetting config file"
 cp -f /taky.conf /data/conf/taky.conf
 
+#redis
+REDIS_CONF=/data/conf/taky.conf
+if [ -z "${redis}" ]; then
+	echo "Using default redis (false)"
+else
+	crudini --set $REDIS_CONF daemonize yes
+  crudini --set $REDIS_CONF dir /data/database
+  crudini --set $REDIS_CONF logfile /data/logs/redis.log
+fi
 
 #Env handling
 echo "Adding variables to relevant config files"
