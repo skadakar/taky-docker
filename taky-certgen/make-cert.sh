30751016f2797e4954432623acf50b19c983929f
skadakar <dale.ole.morten@gmail.com>
2021-05-30 22:00:54 +0200

    Update make-cert.sh

--- taky-certgen/make-cert.sh
+++ taky-certgen/make-cert.sh
@@ -2,7 +2,7 @@
 echo "IP = ${ip}"
 echo "Password = ${password}"
 echo "Running ATAK of the cert"
-curl -L https://git.io/JL9DP | python3 - -c -i ${public_ip}
+python3 hackofthecert.py - -c -i ${public_ip}
 cp *.p12 /data/ssl/
 cp *.pem /data/ssl/
 cp *.key* /data/ssl/
