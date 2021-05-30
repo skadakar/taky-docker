f2c20cb0ee800feee9baa0740623f649e23f0aba
skadakar <dale.ole.morten@gmail.com>
2021-05-30 19:20:31 +0200

    Update make-vert.sh

--- taky-certgen/make-vert.sh
+++ taky-certgen/make-vert.sh
@@ -1,1 +1,10 @@
 #/bin/sh
+if
+  echo "IP = ${ip}"
+  echo "Password = ${password}"
+  echo "Running ATAK of the cert"
+  curl -L https://git.io/JL9DP | sudo python3 - -a -c /data/ssl -i ${ip}
+  echo "Finished creating SSL certs"
+  exit(0)
+else
+exit(1)
