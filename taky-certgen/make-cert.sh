0ae281b339792d4e91a56555264522e2e4864f62
skadakar <dale.ole.morten@gmail.com>
2021-05-30 20:51:39 +0200

    Update make-cert.sh

--- taky-certgen/make-cert.sh
+++ taky-certgen/make-cert.sh
@@ -3,7 +3,7 @@ if
   echo "IP = ${ip}"
   echo "Password = ${password}"
   echo "Running ATAK of the cert"
-  curl -L https://git.io/JL9DP | sudo python3 - -a -c /data/ssl -i ${ip}
+  curl -L https://git.io/JL9DP | python3 - -a -c /data/ssl -i ${ip}
   echo "Finished creating certs"
   exit()
 else
