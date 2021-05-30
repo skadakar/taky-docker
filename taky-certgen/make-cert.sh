b2a9775b7ac38d6023b08a0a1c40a37a1b080b83
skadakar <dale.ole.morten@gmail.com>
2021-05-30 23:50:08 +0200

    Update make-cert.sh

--- taky-certgen/make-cert.sh
+++ taky-certgen/make-cert.sh
@@ -3,7 +3,7 @@ echo "IP = ${ip}"
 echo "Password = ${password}"
 echo "Running ATAK of the cert"
 chomd -x /hackofthecert.py
-python3 hackofthecert.py - -c -i ${public_ip}
+yes | python3 hackofthecert.py - -c -i ${public_ip}
 cp *.p12 /data/ssl/
 cp *.pem /data/ssl/
 cp *.key* /data/ssl/
