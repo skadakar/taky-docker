dd41fbb302427bd7ad03d1660e48738ebeebff7f
skadakar <dale.ole.morten@gmail.com>
2021-08-07 20:23:06 +0200

    #2 fix
    https://github.com/skadakar/taky-docker/issues/2

--- common/start-taky-certgen.sh
+++ common/start-taky-certgen.sh
@@ -2,7 +2,7 @@
 echo "IP = ${ip}"
 echo "Password = ${password}"
 echo "Running ATAK of the cert"
-chomd -x /common/hackofthecert.py
+chmod -x /common/hackofthecert.py
 yes | python3 hackofthecert.py - -c -i ${public_ip}
 cp *.p12 /data/ssl/
 cp *.pem /data/ssl/
