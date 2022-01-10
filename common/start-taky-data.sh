099d243ac634de47e4931c3df745b2482023dca1
skadakar <dale.ole.morten@gmail.com>
2022-01-10 23:23:07 +0100

    Update start-taky-data.sh

--- common/start-taky-data.sh
+++ common/start-taky-data.sh
@@ -2,7 +2,6 @@
 echo "chownhack"
 chown -R taky:taky /data
 
-
 echo "Preparing folder structure"
 mkdir -p /data/conf
 mkdir -p /data/ssl
@@ -11,19 +10,5 @@ mkdir -p /data/upload
 mkdir -p /data/upload/meta
 mkdir -p /data/database
 
-#redis
-#if [ -z "${redis}" ]; then
-#	echo "Using default redis (false)"
-#else
-#	#do nerdshit here
-#	redis-server --daemonize yes
-#fi
-
-#Starting taky
-#echo "Debug"
-#echo " "
-#echo "Running the following config"
-#echo "$(cat /data/conf/taky.conf)"
-#echo " "
 echo "Starting taky data package service as taky user"
 taky_dps -c /data/conf/taky.conf -l debug
