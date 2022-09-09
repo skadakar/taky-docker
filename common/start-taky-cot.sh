16e6d96817a11cf3850921e81779dcfbf6826609
skadakar <dale.ole.morten@gmail.com>
2022-09-09 19:09:39 +0200

    Update start-taky-cot.sh

--- common/start-taky-cot.sh
+++ common/start-taky-cot.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
+if [ -z "${ssl_enabled}" ]; then
+	echo "No SSL configured"
+else
+	if [ -f "/data/ssl/ca.crt" ]; then
+    		echo "Certs already configured, skipping."
+	else
+		echo "Generating certs for Taky"
+		takyctl setup --p12_pw=$server_p12_key --host=${hostname}  --bind-ip=${bind_ip} --public-ip=${public_ip}
+		cp /etc/taky/ssl/* /data/ssl/
+		echo "Certificates created, will exit out and restart."
+		exit
+	fi
+
+fi
 #redis
 if ${redis} != False; then
 	if ${redis} '==' True; then
