826c29ea57316b0b9a403b15ce943426500ee42b
skadakar <dale.ole.morten@gmail.com>
2022-09-09 18:58:08 +0200

    Update start-taky-cot.sh

--- common/start-taky-cot.sh
+++ common/start-taky-cot.sh
@@ -1,17 +1,4 @@
 #!/bin/sh
-if [ -z "${ssl_enabled}" ]; then
-	echo "No SSL configured"
-else
-	if [ -f "/data/ssl/server.p12" ]; then
-    		echo "Certs already configured, skipping."
-	else
-		echo "Generating certs for Taky"
-		takyctl setup --public-ip=${public_ip}
-		cp /etc/taky/ssl/* /data/ssl/
-		exit
-	fi
-	
-fi
 #redis
 if ${redis} != False; then
 	if ${redis} '==' True; then
@@ -21,6 +8,5 @@ if ${redis} != False; then
 	else
 	echo "Redis is enabled and will try using the connectionstring" + ${redis}
 fi
-
 echo "Starting taky COT as taky user"
 taky -c /data/conf/taky.conf -l debug -d
